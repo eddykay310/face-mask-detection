5b7f3feec3e3c008a2f364b0f152bca68f9c7ec4
eddykay310 <baffour1kyei@gmail.com>
2020-09-06 22:29:44 +0000

    creating react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
+import Rank from './components/rank/Rank'
 import './App.css';
+import Particles from 'react-particles-js';
+
+
+const particlesProps =
+{
+  "particles": {
+    "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }
+  },
+  "opacity": { "value": 0.7, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } },
+  "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } },
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+        params={particlesProps} />
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <Rank />
+      {/*
+      
+      <FaceRecognition/> */}
     </div>
   );
 }
